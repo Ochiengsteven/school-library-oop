18c5a520c322f13cdf57ec33ad7818b10e2045a6
Ochiengsteven <stevenochieng6305@gmail.com>
2023-11-08 11:54:49 +0300

    add classs student with attributes and methods

--- person.rb
+++ person.rb
@@ -1,11 +1,26 @@
+require 'securerandom'
+
 class Person
   attr_accessor :name, :age, :parent_permission
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permision: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
-    @id = id
-    @parent_permision = parent_permision
+    @id = SecureRandom.uuid
+    @parent_permission = parent_permission
+  end
+
+  def can_use_service
+    of_age || parent_permission
+  end
+
+  private
+
+  def of_age
+    age >= 18
   end
 end
+
+student1 = Person.new(20, 'Steve', parent_permission: false)
+puts student1.can_use_service
