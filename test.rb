7f5839b0f37ba4937dfa245b0e73d76158bbd886
Ochiengsteven <stevenochieng6305@gmail.com>
2023-11-09 14:00:55 +0300

    ADD STUDENT CLASSROOM ASSOCIATION

--- test.rb
+++ test.rb
@@ -1,9 +1,26 @@
 require_relative 'person'
 require_relative 'nameable'
+require_relative 'classroom'
+require_relative 'student'
 
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+student1 = Student.new(21, 'Jack')
+student3 = Student.new(16, 'Mary')
+
+class1 = Classroom.new('Math')
+class1.students
+
+student1.classroom
+class1.add_student(student1)
+student1.classroom
+student1.classroom.label
+class1.students
+
+student3.classroom
+class1.add_student(student3)
+student3.classroom
+student3.classroom.label
+class1.students
+
+puts class1.students.count
+puts class1.students.first.name
+puts class1.students.last.age
